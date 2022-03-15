aa3b7aa66664ea20f80e066bf258fc563276b21b
aureline12 <motouomaureline@gmail.com>
2022-03-15 13:51:52 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet_x10(void)
+{
+        char n;
+	int i=0;
+	
+	 while (i<10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+
+}
+
