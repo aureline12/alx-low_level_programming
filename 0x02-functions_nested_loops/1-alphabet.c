ff7e848848a18f57d8fab6772d3970d3afc29fc3
aureline12 <motouomaureline@gmail.com>
2022-03-15 13:37:29 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	 for (char c = 'a'; c <= 'z'; c++)
+	 {
+        	_putchar(c);
+	 }
+	_putchar('\n');
+	return (0);
+}
+
+
