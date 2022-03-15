7029d670367c76c2449632f28ab8abc92914ada1
aureline12 <74280873+aureline12@users.noreply.github.com>
2022-03-15 22:04:09 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
         char n;
 	int i=0;
 	
-	 while (i<10)
+	while (i < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
-		i=i+1;
+		i++;
 		_putchar('\n');
 	}
 
