be7782569efffe70445608c7bc338b3f69215716
aureline12 <74280873+aureline12@users.noreply.github.com>
2022-05-02 10:38:36 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,47 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * binary_to_uint - function that prints the binary representation of a number.
- * @b: Binary to print
- * Return: Result number.
- */
+  * _stoi - converts chars to ints
+  * @c: char to convert
+  * Return: converted int
+  */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+  * _strlen - calculates the length of the string
+  * @s: input
+  * Return: length of string
+  */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+  * binary_to_uint - converts a string of 1's and 0's to a decimal number
+  * @b: string to convert
+  * Return: unsigned decimal number
+  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, length, decimal;
+	int i;
+	unsigned int result, tmp, expo;
 
-	i = length = decimal = 0;
-	if (b == NULL)
+	if (!b)
 		return (0);
-	/* We obtain the length of the string */
-	while (b[length] != '\0')
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 	{
-		if (b[length] == '0' || b[length] == '1')
-			length++;
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
 	}
-
-	while (i < length)
-	{
-		decimal = decimal << 1;
-		if (b[i] == '1')
-			decimal += 1;
-		i++;
-	}
-	return (decimal);
+	return (result);
 }
+
